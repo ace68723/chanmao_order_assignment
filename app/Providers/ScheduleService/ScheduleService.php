f74a18dcfc8e1d555891c5c3e2aa121811a8f7ec
xunrui-chanmao <xunrui@chanmao.ca>
2018-04-10 02:45:07 +0800

    getting order and drivers

--- app/Providers/ScheduleService/ScheduleService.php
+++ app/Providers/ScheduleService/ScheduleService.php
@@ -17,9 +17,19 @@ class ScheduleService{
         $this->consts['MAXNLOCATIONS'] = 100;
         $this->consts['TIMEZONE'] = 'America/Toronto';
         $this->consts['ORDER_LIVE_HOURS'] = 4;
+        $this->consts['AREA'] = [
+            1=>'SC',
+            2=>'NY',
+            3=>'RH',
+            4=>'MH',
+            5=>'MH',
+            6=>'DT',
+            10=>'MI',
+        ];
     }
 
-    private function getOrders() {
+    private function getOrders($area) {
+        $timer = -time(true);
         $dt = new \DateTime($this->consts['ORDER_LIVE_HOURS'].' hour ago',
             new \DateTimeZone($this->consts['TIMEZONE']));
         $whereCond = [
@@ -27,6 +37,7 @@ class ScheduleService{
             ['ob.created', '>', $dt->format('Y-m-d H:i:s')],
             ['ob.dltype','>=', 1], ['ob.dltype','<=', 3], ['ob.dltype', '!=', 2],
             ['rl.area','=',0],
+            ['rb.area','=',$area],
         ];
         $res = DB::table('cm_order_base as ob')
             ->join('cm_rr_base as rb', 'rb.rid','=','ob.rid')
@@ -34,58 +45,33 @@ class ScheduleService{
             ->join('cm_user_address as ua', 'ua.uaid','=','ob.uaid')
             ->join('cm_order_trace as ot', 'ot.oid','=','ob.oid')
             ->select('ob.oid', 'ob.rid', 'ob.uid', 'ob.uaid', 'ob.pptime',
+                'ot.driver_id', 'ot.assign', 'ot.pickup',
                 'rb.area','rl.rr_la as rr_lat', 'rl.rr_lo as rr_lng', 'rb.addr as rr_addr',
                 'ua.addr as user_addr','ua.loc_la as user_lat','ua.loc_lo as user_lng')
             ->where($whereCond)->get();
+        $timer += time(true);
+        Log::debug("got ".count($res)." orders for area $area". " takes:".$timer." secs");
         return $res;
     }
-    private function getDrivers() {
-        $data = [];
-        $res = do_curl('https://www.chanmao.ca/index.php?r=MobAly10/DriverLoc', $data, 'GET');
-        return $data['drivers']??null;
+    private function getDrivers($area) {
+        if (!isset($this->consts['AREA'][$area])) {
+            Log::debug("unknown area code:$area");
+            return [];
+        }
+        $data = do_curl('https://www.chanmao.ca/index.php?r=MobAly10/DriverLoc', $data, 'GET');
+        $drivers = $data['drivers']??[];
+        $ret = [];
+        foreach($drivers as $dr)
+            if ($dr['area'] == $this->consts['AREA'][$area]){
+                $ret[] = $dr;
+            }
+        Log::debug("got ".count($ret)." drivers for area $area". " takes:".$timer." secs");
+        return $ret;
     }
-    public function reload() {
-        $orders = $this->getOrders();
-        $drivers = $this->getDrivers();
+    public function reload($area) {
+        $orders = $this->getOrders($area);
+        $drivers = $this->getDrivers($area);
         return ['orders'=>$orders, 'drivers'=>$drivers];
-    }
-    public function get_today_summary($account_id) {
-        $mchinfo = $this->get_merchant_info_by_id($account_id);
-        $now = time();
-        $start_time = $now - 24*3600;
-        $dt->setTimestamp($start_time);
-        $data = $this->sp_oc->query_txns_by_time($account_id, $start_time, $now, 1, -1);
-        $dict = [];
-        foreach($data['recs'] as $txn) {
-            $dict[$txn['ref_id']] = $txn;
-        }
-        $total = $refund = $tips = 0;
-        foreach($data['recs'] as $txn) {
-            if (!$txn['is_refund']) {
-                $total += $txn['txn_fee_in_cent'];
-                $tips += $txn['tips'] ?? 0;
-            }
-            else {
-                $refund += $txn['txn_fee_in_cent'];
-                $out_trade_no = $txn['txn_link_id'] ?? null;
-                if (is_null($out_trade_no) && substr($txn['ref_id'],-2)=='R1') {
-                    $out_trade_no = substr($txn['ref_id'], 0, -2);
-                    Log::debug(__FUNCTION__.' utilizing generate_txn_ref_id rule. ref_id:'
-                        .$txn['ref_id'].' out_trade_no:'.$out_trade_no);
-                }
-                $tips -= $dict[$out_trade_no]['tips'] ?? 0;
-            }
-        }
-        $amount_due = $total - $refund;
-        return [
-            'start_time'=>$dt->format('Y-m-d H:i:s'),
-            'total'=>$total/100,
-            'count'=>count($data['recs']),
-            'refund'=>$refund/100,
-            'purchase'=>($amount_due-$tips)/100,
-            'tips'=>$tips/100,
-            'amount_due'=>$amount_due/100,
-        ];
     }
 
 }
--- app/Providers/ScheduleService/lib/mycurl.php
+++ app/Providers/ScheduleService/lib/mycurl.php
@@ -1,33 +1,28 @@
 <?php
 
-function do_curl($url, &$data, $method, $timeout=30, $payload=null)
+function do_curl($url, $method, $timeout=30, $payload=null)
 {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HEADER, FALSE);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        if (strtoupper($method) == 'POST') {
-            //post提交方式
-            curl_setopt($ch, CURLOPT_POST, TRUE);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-        }
-        $data = curl_exec($ch);
-        if($data !== false){
-            $ret_code = curl_getinfo($ch);
-            if ($ret_code['http_code'] != 200) {
-                Log::debug("got http_code:".$ret_code['http_code']);
-                return false;
-            }
-            curl_close($ch);
-            $data = json_decode($data, true);
-            return true;
-        }
-        else {
-            $error = curl_errno($ch);
-            curl_close($ch);
-            Log::debug("curl error no: $error");
-        }
-        return false;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    if (strtoupper($method) == 'POST') {
+        //post提交方式
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
     }
+    $data = curl_exec($ch);
+    if($data !== false){
+        //$ret_code = curl_getinfo($ch); if ($ret_code['http_code'] != 200) throw new Exception();
+        curl_close($ch);
+        $data = json_decode($data, true);
+    }
+    else {
+        $error = curl_errno($ch);
+        curl_close($ch);
+        throw new Exception("curl error no: $error");
+    }
+    return $data;
+}
