b8270d7a04c3f443df437cd0de1c8a430dfa391d
xunrui-chanmao <xunrui@chanmao.ca>
2018-04-10 01:41:31 +0800

    get orders

--- app/Providers/ScheduleService/ScheduleService.php
+++ app/Providers/ScheduleService/ScheduleService.php
@@ -15,15 +15,34 @@ class ScheduleService{
     {
         $this->consts = array();
         $this->consts['MAXNLOCATIONS'] = 100;
+        $this->consts['TIMEZONE'] = 'America/Toronto';
+        $this->consts['ORDER_LIVE_HOURS'] = 4;
     }
 
     private function getOrders() {
-        return [];
+        $dt = new \DateTime($this->consts['ORDER_LIVE_HOURS'].' hour ago',
+            new \DateTimeZone($this->consts['TIMEZONE']));
+        $whereCond = [
+            ['ob.status','>=', 10], ['ob.status','<=', 30],
+            ['ob.created', '>', $dt->format('Y-m-d H:i:s')],
+            ['ob.dltype','>=', 1], ['ob.dltype','<=', 3], ['ob.dltype', '!=', 2],
+            ['rl.area','=',0],
+        ];
+        $res = DB::table('cm_order_base as ob')
+            ->join('cm_rr_base as rb', 'rb.rid','=','ob.rid')
+            ->join('cm_rr_loc as rl', 'rl.rid','=','rb.rid')
+            ->join('cm_user_address as ua', 'ua.uaid','=','ob.uaid')
+            ->join('cm_order_trace as ot', 'ot.oid','=','ob.oid')
+            ->select('ob.oid', 'ob.rid', 'ob.uid', 'ob.uaid', 'ob.pptime',
+                'rb.area','rl.rr_la as rr_lat', 'rl.rr_lo as rr_lng', 'rb.addr as rr_addr',
+                'ua.addr as user_addr','ua.loc_la as user_lat','ua.loc_lo as user_lng')
+            ->where($whereCond)->get();
+        return $res;
     }
     private function getDrivers() {
         $data = [];
         $res = do_curl('https://www.chanmao.ca/index.php?r=MobAly10/DriverLoc', $data, 'GET');
-        return $data;
+        return $data['drivers']??null;
     }
     public function reload() {
         $orders = $this->getOrders();
@@ -32,7 +51,6 @@ class ScheduleService{
     }
     public function get_today_summary($account_id) {
         $mchinfo = $this->get_merchant_info_by_id($account_id);
-        $dt = new \DateTime('now', $this->create_datetimezone($mchinfo['timezone']??null));
         $now = time();
         $start_time = $now - 24*3600;
         $dt->setTimestamp($start_time);
